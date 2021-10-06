621255ff7209aa8e2dbecec43acd87e1bce2c273
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-10-06 19:43:38 +0330

    BUTTON: Implement unit test for button component

--- src/components/button/__test__/button.test.tsx
+++ src/components/button/__test__/button.test.tsx
@@ -7,41 +7,14 @@ describe('Button component', () => {
     const child = screen.getByText(/Click me/i)
     expect(child).toBeVisible()
   })
-  it('Should be disabled if disabled props is true', () => {
-    render(<Button disabled>Click me</Button>)
-    const child = screen.getByRole('button')
-    expect(child.className).toContain('opacity-50')
-  })
-  it('Should be loading if loading props is true', () => {
-    render(<Button loading>Click me</Button>)
+  it('Should change cursor on fetching', () => {
+    render(<Button fetching>Click me</Button>)
     const child = screen.getByRole('button')
     expect(child.className).toContain('cursor-wait')
   })
-  it('Should not have shadow if children is icon', () => {
-    render(<Button icon>Click me</Button>)
+  it('Should be loading if fetching props is true', () => {
+    render(<Button fetching>Click me</Button>)
     const child = screen.getByRole('button')
-    expect(child.className).not.toContain('shadow')
-  })
-  it('Should not have transform if is disabled', () => {
-    render(<Button disabled>Click me</Button>)
-    const child = screen.getByRole('button')
-    expect(child.className).not.toContain(
-      'transform hover:-translate-y-1 hover:scale-105'
-    )
-  })
-  it('Should contain classname props', () => {
-    render(<Button className="bg-indigo-500">Click me</Button>)
-    const child = screen.getByRole('button')
-    expect(child.className).toContain('bg-indigo-500')
-  })
-  it('Should render loading bounce when loading props is true', () => {
-    render(<Button loading>Click me</Button>)
-    const child = screen.getByTestId('loading-bounce')
-    expect(child).toBeInTheDocument()
-  })
-  it('Should not render children when its loading', () => {
-    render(<Button loading>Click me</Button>)
-    const child = screen.queryByText(/Click me/i)
-    expect(child).not.toBeInTheDocument()
+    expect(child).toHaveTextContent('Loading ...')
   })
 })
