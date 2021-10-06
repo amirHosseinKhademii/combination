2ce3aa6752a6726705c448ff0698c34c03fea33f
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-10-06 18:25:11 +0330

    USE_FETCH: Implement use fetch test

--- src/hooks/use-fetch/__test__/use-fetch.test.ts
+++ src/hooks/use-fetch/__test__/use-fetch.test.ts
@@ -1,9 +1,8 @@
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks'
 import { useFetch } from '..'
-import { wrapper } from 'tests'
-import { getPlaceholder } from 'services/placeholder'
+import { getUser } from 'services/placeholder'
 
-const service = getPlaceholder as any
+const service = getUser as any
 
 jest.mock('services/placeholder')
 
@@ -12,12 +11,26 @@ describe('Use fetch hook', () => {
   afterEach(() => jest.resetAllMocks())
 
   it('Should render properly', async () => {
-    const { result, waitFor } = renderHook(
-      () => useFetch({ key: 'test', service }),
-      { wrapper }
-    )
+    const { result, waitFor } = renderHook(() => useFetch({ service }))
     expect(result.current.loading).toBeTruthy()
     await waitFor(() => !result.current.loading)
+    expect(service).toHaveBeenCalledTimes(1)
+    expect(service).toHaveBeenCalledWith()
     expect(result.current.data.data).toBe('test')
+  })
+
+  it('Should refetch properly', async () => {
+    const { result, waitFor } = renderHook(() => useFetch({ service }))
+    expect(result.current.loading).toBeTruthy()
+    await waitFor(() => !result.current.loading)
+    expect(result.current.loading).toBeFalsy()
+    act(() => result.current.refetch())
+    jest.spyOn(console, 'error').mockImplementation(jest.fn())
+    expect(service).toHaveBeenCalledTimes(2)
+    expect(service).toHaveBeenCalledWith()
+    jest.resetAllMocks()
+    expect(result.current.fetching).toBeTruthy()
+    await waitFor(() => !result.current.fetching)
+    expect(result.current.fetching).toBeFalsy()
   })
 })
