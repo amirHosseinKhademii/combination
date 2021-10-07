6e4c442dedff1666e5af7a081eb68d9bada2045d
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-10-07 14:44:44 +0330

    USER: Improve use user and user vicew integration and unit test

--- src/pages/user/use-user/__test__/use-user.test.ts
+++ src/pages/user/use-user/__test__/use-user.test.ts
@@ -1,49 +1,41 @@
 import { renderHook, act } from '@testing-library/react-hooks'
 import { getUser } from 'services/placeholder'
+import { mockData, mockNewData } from 'tests/mock'
 import { useUser } from '..'
 
 const service = getUser as any
 
-const mockData = {
-  data: {
-    results: [
-      {
-        name: { first: 'amir', last: 'khademi' },
-        email: 'user@',
-        picture: { large: 'some img' },
-      },
-    ],
-  },
-}
-
 jest.mock('services/placeholder')
 
 describe('Use user hook', () => {
-  beforeEach(() => service.mockResolvedValueOnce(mockData))
   afterEach(() => jest.resetAllMocks())
 
   it('Should render proper data', async () => {
+    service.mockResolvedValueOnce(mockData)
     const { result, waitFor } = renderHook(() => useUser())
     expect(result.current.loading).toBeTruthy()
     await waitFor(() => !result.current.loading)
     expect(service).toHaveBeenCalledTimes(1)
     expect(result.current.data).toBe(mockData)
   })
-  it('Should refetch proper data', async () => {
+  it('Should refetch proper data and catch the diff between email and img', async () => {
+    service.mockResolvedValueOnce(mockData)
     const { result, waitFor, waitForNextUpdate } = renderHook(() => useUser())
     await waitFor(() => !result.current.loading)
     expect(result.current.cache).toBe(undefined)
+    jest.resetAllMocks()
+    service.mockResolvedValueOnce(mockNewData)
     act(() => result.current.refetch())
     jest.spyOn(console, 'error').mockImplementation(jest.fn())
     expect(result.current.fetching).toBeTruthy()
     await waitForNextUpdate()
-    expect(service).toHaveBeenCalledTimes(2)
     expect(result.current.cache).toBe(mockData)
+    expect(result.current.data).toBe(mockNewData)
     expect(result.current.isNew).toStrictEqual({
-      email: undefined,
-      first: undefined,
-      img: undefined,
-      last: undefined,
+      email: true,
+      first: false,
+      img: true,
+      last: false,
     })
   })
 })
